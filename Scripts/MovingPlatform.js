cd81bb2ab828d48c7d7fd339590109cfb4572817
Lucas <lucasnar@gmail.com>
2015-03-31 19:01:09 -0400

    Add MovingPlatform.js
    When a platform moves, it needs some special code for the player sprite
    to behave correctly.

---
+++ Scripts/MovingPlatform.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D(other : Collider2D) 
+{ 
+	other.transform.parent = gameObject.transform;
+} 
+
+function OnTriggerExit2D(other : Collider2D) 
+{
+	other.transform.parent = null;
+}
