ea6451be459f3edec89e33c23480c1b5b0d9218e
Lucas <lucasnar@gmail.com>
2015-04-16 14:51:24 -0400

    Update HitBox.js
    Character goes to the second scene when opening the door and other minor
    changes.

--- Scripts/HitBox.js
+++ Scripts/HitBox.js
@@ -1,10 +1,16 @@
-﻿#pragma strict
+﻿#pragma downcast
+
+var player : GameObject;
 
 function OnTriggerEnter2D(other : Collider2D){
 
-	if(other.tag == "Door1")
+	var keyScript : MovePlayer = player.GetComponent(MovePlayer);
+
+	if(other.tag == "Door1" && keyScript.key1obtained)
 	{
 		Destroy(other.gameObject);
+		gameObject.Find("DoorSound").audio.Play();
+		gameObject.Find("Key_1Image").GetComponent(UI.Image).color.a = 0;
+		Application.LoadLevel("Scene_2");
 	}
-
 }
