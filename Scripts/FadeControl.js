c0a22198d9709532a5845f44dc6d46791cc93391
Lucas <lucasnar@gmail.com>
2015-04-16 14:54:36 -0400

    Add FadeControl.js
    In order for the menu (or other chosen scene) to have a smooth
    transition, I made this script, which makes a image fade out in its
    alpha channel.

---
+++ Scripts/FadeControl.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+	while (gameObject.GetComponent(UI.Image).color.a >	 0){
+	
+		gameObject.GetComponent(UI.Image).color.a -= 0.1;
+		yield WaitForSeconds(0.13);
+	
+	}
+	Destroy(gameObject);
+
+}
