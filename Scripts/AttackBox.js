a9a181924d4701c266af313fa631ac75822d123f
Lucas <lucasnar@gmail.com>
2015-04-16 14:58:55 -0400

    Update AttackBox.js and MovingPlatform.js
    Minor changes.

--- Scripts/AttackBox.js
+++ Scripts/AttackBox.js
@@ -5,6 +5,7 @@ function OnTriggerEnter2D(other : Collider2D){
 	if(other.tag == "Enemy")
 	{
 		Destroy(other.gameObject);
+		gameObject.Find("HurtSound").audio.Play();	
 	}
 
 }
--- Scripts/MovingPlatform.js
+++ Scripts/MovingPlatform.js
@@ -2,10 +2,12 @@
 
 function OnTriggerEnter2D(other : Collider2D) 
 { 
-	other.transform.parent = gameObject.transform;
+	// other.transform.parent = gameObject.transform;
+	other.transform.SetParent(gameObject.transform, true);
 } 
 
 function OnTriggerExit2D(other : Collider2D) 
 {
-	other.transform.parent = null;
+	// other.transform.parent = null;
+	other.transform.SetParent(null, true);
 }
