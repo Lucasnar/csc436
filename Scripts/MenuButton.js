bc35b5f146effdae127aac1273bbb1004183c3c7
Lucas <lucasnar@gmail.com>
2015-04-18 05:47:08 -0400

    Update MenuButton.js
    I had problems when trying to load the first scene after pausing the
    game, since the character persists between scenes and wasn't destroyed
    by the Die() function. .

--- Scripts/MenuButton.js
+++ Scripts/MenuButton.js
@@ -3,14 +3,20 @@
 function OnMouseDown(){
 	
 	if (gameObject.name == "StartButton"){
-	
+		
+		if (gameObject.Find("Warrior_0")){
+		
+			Destroy(gameObject.Find("Warrior_0"));
+		
+		}
+		
 		Application.LoadLevel("Scene_1");
 		Time.timeScale = 1;
 	
 	}
 	
 	if (gameObject.name == "QuitButton"){
-	
+		
 		Application.Quit();
 	
 	}
