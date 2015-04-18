abf22d92706563e86f0d512e420209b1bcead20f
Lucas <lucasnar@gmail.com>
2015-04-18 05:49:29 -0400

    Update HitBox.js
    Of course I have to control the key1obtained variable here, when it hits
    and opens the door. I was turning it to false when the player pressed
    the button.

--- Scripts/HitBox.js
+++ Scripts/HitBox.js
@@ -9,6 +9,8 @@ function OnTriggerEnter2D(other : Collider2D){
 	if(other.tag == "Door1" && keyScript.key1obtained)
 	{
 		Destroy(other.gameObject);
+		
+		keyScript.key1obtained = false;
 		gameObject.Find("DoorSound").audio.Play();
 		gameObject.Find("Key_1Image").GetComponent(UI.Image).color.a = 0;
 		Application.LoadLevel("Scene_2");
