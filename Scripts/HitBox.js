8394b3d65eaa7d47cffa9fbc0c5976dbe6401df2
Lucas <lucasnar@gmail.com>
2015-04-20 05:57:47 -0400

    Update HitBox.js
    I didn't expect this code to get much bigger than it was, but as I was
    adding the dialogs, I was realizing that I should make a lot of changes
    to it.

--- Scripts/HitBox.js
+++ Scripts/HitBox.js
@@ -5,14 +5,36 @@ var player : GameObject;
 function OnTriggerEnter2D(other : Collider2D){
 
 	var keyScript : MovePlayer = player.GetComponent(MovePlayer);
+	var enterDoorText : GameObject = gameObject.Find("EnterDoorText");
 
 	if(other.tag == "Door1" && keyScript.key1obtained)
 	{
-		Destroy(other.gameObject);
 		
+		Destroy(other.gameObject);
+		enterDoorText.GetComponent(UI.Text).color.a = 1;
 		keyScript.key1obtained = false;
 		gameObject.Find("DoorSound").audio.Play();
 		gameObject.Find("Key_1Image").GetComponent(UI.Image).color.a = 0;
-		Application.LoadLevel("Scene_2");
+		keyScript.StopControl();
+		yield WaitForSeconds(5);
+		
+		if (Application.loadedLevelName == "Scene_1") {
+		
+			Application.LoadLevel("Scene_2");
+		
+		}
+		
+		if (Application.loadedLevelName == "Scene_2") {
+		
+			
+			keyScript.EnableButtons();
+			yield WaitForSeconds(600);
+			keyScript.DisableButtons();
+			gameObject.Find("Warrior_0").transform.position = Vector3(100, 100, 100);
+			gameObject.Find("AreYouStillHereText").GetComponent(UI.Text).color.a = 1;
+			enterDoorText.GetComponent(UI.Text).color.a = 0;
+		}
+		
+		keyScript.StartControl();
 	}
 }
