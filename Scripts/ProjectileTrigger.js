29c5a828db9de3c7cc26344112616d55060efdff
Lucas <lucasnar@gmail.com>
2015-05-09 18:26:54 -0400

    Add ProjectileTrigger
    Added this code to a projectile to control when the other one starts.

---
+++ Scripts/ProjectileTrigger.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D(other : Collider2D){
+
+	if (other.gameObject.tag == "ActiveSecondProjectile"){
+		
+		gameObject.Find("Projectile2").GetComponent(Animator).SetBool("ProjectileActive", true);
+	}
+}
