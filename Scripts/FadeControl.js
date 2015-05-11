f328596798d63f7db5c8af9fd0eedd407749400f
Lucas <lucasnar@gmail.com>
2015-05-11 03:25:48 -0400

    Update FadeControl.js and MenuButton.js
    Minor changes and a failed attempt to add a restart scene button.

--- Scripts/FadeControl.js
+++ Scripts/FadeControl.js
@@ -2,10 +2,10 @@
 
 function Start () {
 
-	while (gameObject.GetComponent(UI.Image).color.a >	 0){
+	while (gameObject.GetComponent(UI.Image).color.a > 0){
 	
 		gameObject.GetComponent(UI.Image).color.a -= 0.1;
-		yield WaitForSeconds(0.13);
+		yield WaitForSeconds(0.1);
 	
 	}
 	Destroy(gameObject);
--- Scripts/MenuButton.js
+++ Scripts/MenuButton.js
@@ -1,6 +1,9 @@
 ﻿#pragma strict
+//var player : GameObject;
 
 function OnMouseDown(){
+
+	//var keyScript : MovePlayer = player.GetComponent(MovePlayer);
 	
 	if (gameObject.name == "StartButton"){
 		
@@ -13,12 +16,30 @@ function OnMouseDown(){
 		Application.LoadLevel("Scene_1");
 		Time.timeScale = 1;
 	
-	}
+	}/*
 	
-	if (gameObject.name == "QuitButton"){
+	else if (gameObject.name == "RestartSceneButton"){
+		
+		if (Application.loadedLevelName == "Scene_1"){
+			if (gameObject.Find("Warrior_0")){
+		
+				Destroy(gameObject.Find("Warrior_0"));
+			}
+		}
+		
+		Application.LoadLevel(Application.loadedLevelName);
+	
+	}*/
+	
+	else if (gameObject.name == "QuitButton"){
 		
 		Application.Quit();
+		#if UNITY_EDITOR
+     	UnityEditor.EditorApplication.isPlaying = false;
+     	#endif
 	
 	}
+	
+	
 
 }
